cf1bad9772efe3db4b9a46c7fce5aeb4608e2c11
Lucas Manolli <lucas.manolli.sfs@gmail.com>
2023-07-25 13:55:14 -0300

    ajuste do menu para apresentar somente as opções tarefas e tarefas-live

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -9,16 +9,9 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
-
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
-
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
-
-                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Reports</a>
-                        </div>
+                            <a href="{{ route('tarefas.index') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Tarefas</a>
+                            <a href="{{ route('tarefas-livewire.index') }}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Tarefas-livewire</a>
+                        </div> 
                     </div>
                 </div>
                 <div class="hidden md:block">
